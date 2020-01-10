6aa75808477b9d1501e777775377dae8da61fe9f
akrawczyk <akrawczyk@relayonit.pl>
2020-01-10 12:16:36 +0100

    tabela

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { TestComponent } from './test/test.component';
+import { MenuComponent } from './menu/menu.component';
+import { ItemComponent } from './item/item.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TestComponent,
+    MenuComponent,
+    ItemComponent,
+    WelcomeComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule
@@ -13,4 +23,5 @@ import { AppComponent } from './app.component';
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
