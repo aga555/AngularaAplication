46bc388e672de981efa2436658855380dd23e0ba
akrawczyk <akrawczyk@relayonit.pl>
2020-01-26 07:50:21 +0100

    # *ng-if + hidden- drzew dom

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,22 +9,16 @@ export class AppComponent {
 
   city = 'Warszawa';
   days = ['Poniedzialek', 'Wtorek', 'Środa', 'Czwartek', 'Piątek', 'Sobota', 'Niedziela'];
-  poems = new Array<Poem>();
+  isVisible = true;
+  isHidden = false;
 
-  /*constructor() {
-    this.poems.push(new Poem('Szymborska', 'Poezja'),
-      new Poem('Miłosz', 'Miłość'),
-      new Poem('Twardowski', 'Podziękowanie'),
-      new Poem('Mickiewicz', 'Oda  do Młodości'),
-      new Poem('Białoszewski', 'Piec'));
-  }*/
-
-
-}
-
-class Poem {
-  constructor(public author: string, public title: string) {
-
+  show() {
+    this.isVisible = !this.isVisible;
   }
+
+  hidden() {
+    this.isHidden = !this.isHidden;
+  }
+
 
 }
