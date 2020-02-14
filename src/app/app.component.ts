34591c49d6fbbe5c4e8c5e4fa78301f0e85e7bee
akrawczyk <akrawczyk@relayonit.pl>
2020-02-14 15:00:11 +0100

    #lokal references

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
-import {Component} from '@angular/core';
+import {Component, ElementRef, ViewChildren} from '@angular/core';
+import {ChildComponent} from './child/child.component';
 
 @Component({
   selector: 'app-root',
@@ -9,8 +10,19 @@ export class AppComponent {
 
   placesList = ['New York', 'Los Angeles', 'Tokyo'];
 
+  @ViewChildren('childRef')
+  childComponent: ChildComponent;
+
+  @ViewChildren('inputText')
+  input: ElementRef;
+
   selected(place: string): void {
     console.log(place);
+  }
+
+  add(input: HTMLInputElement) {
+    this.placesList.push(input.value);
+   /* this.childComponent.places = [];*/
   }
 }
 
