a2bf94572d02f7d86abd9dbc48550f82a7d1444e
akrawczyk <agammkrawczyk@gmail.com>
2020-02-27 17:55:03 +0100

    # add  progres bar  and countires

--- src/app/add-place/add-place.component.ts
+++ src/app/add-place/add-place.component.ts
@@ -19,8 +19,28 @@ export class AddPlaceComponent implements OnInit {
   countries: Place[] = [
     {name: 'Poland'},
     {name: 'Germany'},
-    {name: 'Great Britain'}
+    {name: 'Great Britain'},
+    {name: ' Austria'},
+    {name: 'Belgium'},
+    {name: ' Bulgaria'},
+    {name: 'Croatia'},
+    {name: 'Cyprus'},
+    {name: ' Czech Republic'},
+    {name: ' Denmark'},
+    {name: 'Estonia'},
+    {name: ' Finland'}, {name: 'France'}, {name: 'Germany'}, {name: ' Greece'}, {name: 'Hungary'}, {name: ' Ireland'}, {name: ' Italy'},
+    {name: 'Latvia'}, {name: ' Lithuania'}, {name: 'Luxembourg'}, {name: ' Malta'}, {name: 'Netherlands'}, {name: 'Poland'},
+    {name: 'Portugal'},
+    {name: 'Romania'}, {name: ' Slovakia'}, {name: 'Slovenia'}, {name: ' Spain'}, {name: 'Sweden'}, {name: 'Tirana'},
+    {name: 'Andorra'},
+    {name: ' Belarus'},
+    {name: 'Bosnia and Herzegovina '},
+    {name: 'Montenegro'},
+    {name: 'Iceland '}, {name: 'Kazakhstan'}, {name: ' Liechtenstein'}, {name: 'Macedonia'}, {name: 'Monaco'}, {name: 'Norway'},
+    {name: 'Russia '}, {name: 'Serbia '},
+    {name: 'Switzerland'}, {name: 'San Marino'}, {name: 'Ukraine'}, {name: 'Vatican'}
   ];
+
 
   ngOnInit() {
   }
